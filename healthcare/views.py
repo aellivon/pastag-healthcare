7251e44199655e48d31208473be7ac98c758ec2c
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2022-10-24 15:25:06 +0900

    Create logout view

--- healthcare/views.py
+++ healthcare/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth import logout
+from django.views.generic import View
+from django.shortcuts import reverse
+from django.http import HttpResponseRedirect
+
+# NOTE: TODO create LoginRequiredMixin
+
+class LogoutView(View):
+    """
+        The logout class
+    """
+    def get(self, *args, **kwargs):
+        logout(self.request)
+        return HttpResponseRedirect(reverse('users:login'))
