2ab8d81b6f751b60dcc46317837cdb41296ebd12
Abegail Isidro <chabbyabe@gmail.com>
2022-10-31 13:10:10 +0900

    Add javascript files, babel compiler and add login js

---
+++ static/non-vendors/js/src/app.js
@@ -0,0 +1,5 @@
+// Import all vendors here
+
+import '/static/jquery/dist/jquery.min.js';
+import '/static/popper.js/dist/popper.min.js';
+import '/static/bootstrap/dist/js/bootstrap.bundle.min.js';
